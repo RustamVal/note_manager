f71c2b44612d9cebb56b1119858009a85fe0a4bc
RustamVal <valiakhmetovri@yandex.ru>
2024-12-22 08:20:44 +0500

    Add files via upload
    Файл с первым заданием.
    Создание и инициализация шести переменных для проекта

---
+++ greetings.py
@@ -0,0 +1,11 @@
+username = "Leonardo"
+title = "Тестовая заметка"
+content = "Заметка для тестирования функционала и возможностей"
+status = "Активная"
+created_date = "22-12-2024"
+issue_date = "29-12-2024"
+print("Имя пользователя: ", username)
+print("Описание заметки: ", content)
+print("Статус заметки: ", status)
+print("Дата создания заметки: ", created_date)
+print("Дедлайн заметки : ", issue_date)
