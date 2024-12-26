fab448b70229b2dbfeb87c98b0ad3b1ce3528e42
RustamVal <valiakhmetovri@yandex.ru>
2024-12-26 23:54:58 +0500

    Создание и вывод списка для заметки
    Grade 1. Этап 1: Задание 5

---
+++ final.py
@@ -0,0 +1,15 @@
+username = input("Введите имя пользователя: ")
+title1 = input("Введите основной заголовок заметки: ")
+title2 = input("Введите 2-й заголовок: ")
+title3 = input("Введите 3-й заголовок: ")
+content = input("Введите содержание заметки: ")
+status = input("Укажите статус заметки (новая, выполняется, выполнена): ")
+created_date = input("Укажите дату создания заметки в формате dd-mm-yyyy: ")
+issue_date = input("Укажите плановую дату создания заметки в формате dd-mm-yyyy: ")
+
+title_lst = [title1, title2, title3]
+
+note = [username, content, status, created_date, issue_date, title_lst]
+
+#print(username,title1,title2,title3,content,status,created_date,issue_date)
+print(note)
