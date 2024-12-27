ebbddf15944437e01fc474ea13a1a24f5822a45f
RustamVal <valiakhmetovri@yandex.ru>
2024-12-27 10:01:19 +0500

    Update date_changer.py

--- date_changer.py
+++ date_changer.py
@@ -1,4 +1,10 @@
 created_date = "22-12-2024"
 issue_date = "29-12-2024"
+
+#вариант 1
 print(f"Дата создания заметки: {created_date:.5}")
 print(f"Дедлайн заметки: {issue_date:.5}")
+
+#вариант 2
+print("Дата создания заметки: ", created_date[0:5])
+print("Дедлайн заметки: ", issue_date[0:5])
