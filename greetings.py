54b2f1a163b831d6a9d39fc6cace7ed38c08bf35
RustamVal <valiakhmetovri@yandex.ru>
2024-12-27 10:12:58 +0500

    Update greetings.py

--- greetings.py
+++ greetings.py
@@ -5,6 +5,7 @@ status = "Активная"
 created_date = "22-12-2024"
 issue_date = "29-12-2024"
 print("Имя пользователя: ", username)
+print("Заголовок заметки: ", title)
 print("Описание заметки: ", content)
 print("Статус заметки: ", status)
 print("Дата создания заметки: ", created_date)
