de0d3162b5059c5fc01281e7474ee105717a0581
RustamVal <valiakhmetovri@yandex.ru>
2025-01-21 00:36:50 +0500

    Update delete_note.py

--- delete_note.py
+++ delete_note.py
@@ -27,29 +27,31 @@ def print_note(l_note):
 
 # удаление в цикле всех заметок имеющих аналогичные имя пользователя или заголовок
 def delete_note(name_or_title, in_notes):
+    # for l_note in in_notes:
     for i in range(len(in_notes)-1,-1,-1):
         l_note = in_notes[i]
-        if l_note["name"].lower() == name_or_title or l_note["titles"].lower() == name_or_title:
+        if l_note["username"].lower() == name_or_title or l_note["title"].lower() == name_or_title:
             in_notes.remove(l_note)
     #return in_notes
-# блок инициализации значений
 
-notes = fill_notes() # вызов функции заполнения значений заметок
+if __name__ == '__main__':
+    # блок инициализации значений
+    notes = fill_notes() # вызов функции заполнения значений заметок
 
-if len(notes) == 0:
-    print ("Список заметок пуст, удалять нечего")
-else:
-    print("Вот ваш текущий список заметок:")
-    for note in notes:
-        print_note (note)
-
-# блок удаления заметок
-    delete_var = input("Введите имя пользователя или заголовок для удаления заметки (регистр не имеет значения): ")
-    num_notes = len(notes) # запоминаем сколько заметок было до удаления
-    delete_note(delete_var.lower(), notes)
-    if len(notes) != num_notes: # если количество заметок не совпадает выводим информацию сколько удалено
-        print(f"Успешно удалено заметок - {num_notes - len(notes)}. Остались следующие заметки:")
+    if len(notes) == 0:
+        print ("Список заметок пуст, удалять нечего")
+    else:
+        print("Вот ваш текущий список заметок:")
         for note in notes:
-            print_note(note)
-    else: # иначе информируем, что заметок по данным критериям не найдено
-        print("Заметок с таким именем пользователя или заголовком не найдено.")
+            print_note (note)
+
+    # блок удаления заметок
+        delete_var = input("Введите имя пользователя или заголовок для удаления заметки (регистр не имеет значения): ")
+        num_notes = len(notes) # запоминаем сколько заметок было до удаления
+        delete_note(delete_var.lower(), notes)
+        if len(notes) != num_notes: # если количество заметок не совпадает выводим информацию сколько удалено
+            print(f"Успешно удалено заметок - {num_notes - len(notes)}. Остались следующие заметки:")
+            for note in notes:
+                print_note(note)
+        else: # иначе информируем, что заметок по данным критериям не найдено
+            print("Заметок с таким именем пользователя или заголовком не найдено.")
