7463db502db01f73dd27f0c2379255e83310401c
RustamVal <valiakhmetovri@yandex.ru>
2025-01-21 21:59:10 +0500

    Сохранение заметок в файл
    Grade 1. Этап 4: Задание 1

---
+++ save_notes.py
@@ -0,0 +1,33 @@
+def save_notes_to_file(notes, filename):
+    file = open(filename,mode="w", encoding="utf-8")
+    if file.writable():
+        for note in notes:
+            file.write(f"Имя пользователя: {note["username"]}\n")
+            file.write(f"Заголовок: {note["title"]}\n")
+            file.write(f"Описание: {note["content"]}\n")
+            file.write(f"Статус: {note["status"]}\n")
+            file.write(f"Дата создания: {note["created_date"]}\n")
+            file.write(f"Дедлайн: {note["issue_date"]}\n")
+            file.write(f"---\n")
+    else:
+        print(f"Ошибка сохранения. Файл {filename} не открыт для записи")
+    file.close()
+
+def default_notes():
+    in_notes = [
+        {'username': 'Алексей', 'title': 'Список покупок', 'content': 'Купить продукты на неделю', 'status': 'новая',
+         'created_date': '27-11-2024', 'issue_date': '30-11-2024'},
+        {'username': 'Мария', 'title': 'Учеба', 'content': 'Подготовиться к экзамену', 'status': 'в процессе',
+         'created_date': '25-11-2024', 'issue_date': '01-12-2024'},
+        {'username': 'Иван', 'title': 'План работы', 'content': 'Завершить проект', 'status': 'выполнено',
+         'created_date': '20-11-2024', 'issue_date': '26-11-2024'},
+        {'username': 'Георгий', 'title': 'Список дел', 'content': 'Съездить в магазин', 'status': 'в процессе',
+         'created_date': '20-12-2024', 'issue_date': '26-12-2024'},
+        {'username': 'Мария', 'title': 'Список для чтения', 'content': 'Шекспир', 'status': 'выполнено',
+         'created_date': '20-09-2024', 'issue_date': '31-12-2024'}
+    ]
+    return in_notes
+
+if __name__ == '__main__':
+    notes = default_notes()
+    save_notes_to_file(notes,"example.txt")
